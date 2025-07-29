216be5fa1aa48df0433264ee0a8a3e669c269d23
root <root@srv819841.hstgr.cloud>
2025-07-29 12:42:07 +0000

    first

---
+++ mvc/logs/log-2024-11-24.php
@@ -0,0 +1,6 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2024-11-24 10:29:37 --> Severity: error --> Exception: Access denied for user 'root'@'localhost' (using password: YES) /Users/kawsaruddin/Workspace/8.2/school/main/database/drivers/mysqli/mysqli_driver.php 201
+ERROR - 2024-11-24 10:29:37 --> 404 Page Not Found: Faviconico/index
+ERROR - 2024-11-24 10:37:45 --> Severity: error --> Exception: Expression #4 of SELECT list is not in GROUP BY clause and contains nonaggregated column 'school.conversation_user.is_sender' which is not functionally dependent on columns in GROUP BY clause; this is incompatible with sql_mode=only_full_group_by /Users/kawsaruddin/Workspace/8.2/school/main/database/drivers/mysqli/mysqli_driver.php 305
+ERROR - 2024-11-24 10:39:24 --> Severity: error --> Exception: Expression #4 of SELECT list is not in GROUP BY clause and contains nonaggregated column 'school.conversation_user.is_sender' which is not functionally dependent on columns in GROUP BY clause; this is incompatible with sql_mode=only_full_group_by /Users/kawsaruddin/Workspace/8.2/school/main/database/drivers/mysqli/mysqli_driver.php 305
---
+++ mvc/logs/log-2025-07-27.php
@@ -0,0 +1,29 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2025-07-27 16:36:55 --> Severity: Warning --> Uninitialized string offset 0 /Users/kawsaruddin/Workspace/8.2/school/main/database/drivers/mysqli/mysqli_driver.php 120
+ERROR - 2025-07-27 16:36:55 --> Severity: error --> Exception: Access denied for user ''@'localhost' (using password: NO) /Users/kawsaruddin/Workspace/8.2/school/main/database/drivers/mysqli/mysqli_driver.php 201
+ERROR - 2025-07-27 16:36:55 --> 404 Page Not Found: Faviconico/index
+ERROR - 2025-07-27 16:37:21 --> Severity: Warning --> Uninitialized string offset 0 /Users/kawsaruddin/Workspace/8.2/school/main/database/drivers/mysqli/mysqli_driver.php 120
+ERROR - 2025-07-27 16:37:21 --> Severity: error --> Exception: Access denied for user ''@'localhost' (using password: NO) /Users/kawsaruddin/Workspace/8.2/school/main/database/drivers/mysqli/mysqli_driver.php 201
+ERROR - 2025-07-27 16:37:21 --> 404 Page Not Found: Faviconico/index
+ERROR - 2025-07-27 16:37:22 --> Severity: Warning --> Uninitialized string offset 0 /Users/kawsaruddin/Workspace/8.2/school/main/database/drivers/mysqli/mysqli_driver.php 120
+ERROR - 2025-07-27 16:37:22 --> Severity: error --> Exception: Access denied for user ''@'localhost' (using password: NO) /Users/kawsaruddin/Workspace/8.2/school/main/database/drivers/mysqli/mysqli_driver.php 201
+ERROR - 2025-07-27 16:37:22 --> 404 Page Not Found: Faviconico/index
+ERROR - 2025-07-27 16:37:22 --> Severity: Warning --> Uninitialized string offset 0 /Users/kawsaruddin/Workspace/8.2/school/main/database/drivers/mysqli/mysqli_driver.php 120
+ERROR - 2025-07-27 16:37:22 --> Severity: error --> Exception: Access denied for user ''@'localhost' (using password: NO) /Users/kawsaruddin/Workspace/8.2/school/main/database/drivers/mysqli/mysqli_driver.php 201
+ERROR - 2025-07-27 16:37:23 --> 404 Page Not Found: Faviconico/index
+ERROR - 2025-07-27 16:37:23 --> Severity: Warning --> Uninitialized string offset 0 /Users/kawsaruddin/Workspace/8.2/school/main/database/drivers/mysqli/mysqli_driver.php 120
+ERROR - 2025-07-27 16:37:23 --> Severity: error --> Exception: Access denied for user ''@'localhost' (using password: NO) /Users/kawsaruddin/Workspace/8.2/school/main/database/drivers/mysqli/mysqli_driver.php 201
+ERROR - 2025-07-27 16:37:23 --> 404 Page Not Found: Faviconico/index
+ERROR - 2025-07-27 16:37:23 --> Severity: Warning --> Uninitialized string offset 0 /Users/kawsaruddin/Workspace/8.2/school/main/database/drivers/mysqli/mysqli_driver.php 120
+ERROR - 2025-07-27 16:37:23 --> Severity: error --> Exception: Access denied for user ''@'localhost' (using password: NO) /Users/kawsaruddin/Workspace/8.2/school/main/database/drivers/mysqli/mysqli_driver.php 201
+ERROR - 2025-07-27 16:37:23 --> 404 Page Not Found: Faviconico/index
+ERROR - 2025-07-27 16:37:23 --> Severity: Warning --> Uninitialized string offset 0 /Users/kawsaruddin/Workspace/8.2/school/main/database/drivers/mysqli/mysqli_driver.php 120
+ERROR - 2025-07-27 16:37:23 --> Severity: error --> Exception: Access denied for user ''@'localhost' (using password: NO) /Users/kawsaruddin/Workspace/8.2/school/main/database/drivers/mysqli/mysqli_driver.php 201
+ERROR - 2025-07-27 16:37:23 --> 404 Page Not Found: Faviconico/index
+ERROR - 2025-07-27 16:37:23 --> Severity: Warning --> Uninitialized string offset 0 /Users/kawsaruddin/Workspace/8.2/school/main/database/drivers/mysqli/mysqli_driver.php 120
+ERROR - 2025-07-27 16:37:23 --> Severity: error --> Exception: Access denied for user ''@'localhost' (using password: NO) /Users/kawsaruddin/Workspace/8.2/school/main/database/drivers/mysqli/mysqli_driver.php 201
+ERROR - 2025-07-27 16:37:23 --> Severity: Warning --> Uninitialized string offset 0 /Users/kawsaruddin/Workspace/8.2/school/main/database/drivers/mysqli/mysqli_driver.php 120
+ERROR - 2025-07-27 16:37:23 --> Severity: error --> Exception: Access denied for user ''@'localhost' (using password: NO) /Users/kawsaruddin/Workspace/8.2/school/main/database/drivers/mysqli/mysqli_driver.php 201
+ERROR - 2025-07-27 16:37:23 --> 404 Page Not Found: Faviconico/index
+ERROR - 2025-07-27 13:45:07 --> Severity: error --> Exception: Expression #4 of SELECT list is not in GROUP BY clause and contains nonaggregated column 'school.conversation_user.is_sender' which is not functionally dependent on columns in GROUP BY clause; this is incompatible with sql_mode=only_full_group_by /Users/kawsaruddin/Workspace/8.2/school/main/database/drivers/mysqli/mysqli_driver.php 305
